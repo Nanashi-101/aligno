1fa492670270a50c346e8a9720943ee53f52f94f
Nanashi-101 <soumyadipsanyal2017@gmail.com>
2025-11-22 12:31:11 +0100

    Fix server log message to use correct port variable

--- server/src/index.ts
+++ server/src/index.ts
@@ -47,5 +47,5 @@ app.use('/teams', teamRoutes)
 // SERVER
 const port = Number(process.env.PORT) || 5000;
 app.listen(port, "0.0.0.0", () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${port}`);
 });
